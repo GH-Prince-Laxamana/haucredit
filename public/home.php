14de4a0a51bdc8a0db5bde37c0075382e0718f9d
GH-Prince-Laxamana <prince.laxamana116@gmail.com>
2026-02-16 19:37:33 +0800

    improved setup per client

--- public/home.php
+++ public/home.php
@@ -14,7 +14,7 @@ $username = htmlspecialchars($_SESSION["username"], ENT_QUOTES, "UTF-8");
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>HAUCredit - Dashboard</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="../app/css/styles.css" />
 </head>
 <body>
 
