ff26098db6c7bfff22e9bb550b86bc60d6ff2b31
GH-Prince-Laxamana <prince.laxamana116@gmail.com>
2026-02-17 14:15:50 +0800

    fixed script execution for security purposes (csp)

--- public/create_event.php
+++ public/create_event.php
@@ -109,7 +109,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Create Event</title>
-  <link rel="stylesheet" href="../app//layout.css" />
+  <link rel="stylesheet" href="../app/css/layout.css" />
 </head>
 
 <body>
